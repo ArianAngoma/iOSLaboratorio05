394c19b3e0142cfcb6332f31c6b7b04da178cdd8
Arian Angoma <AngomaC15@MacBook-Air-de-Mac.local>
2019-04-10 18:29:27 -0500

    first commit

---
+++ laboratorio05Angoma/laboratorio05Angoma/Tarea.swift
@@ -0,0 +1,14 @@
+//
+//  Tarea.swift
+//  laboratorio05Angoma
+//
+//  Created by Arian Angoma on 5/04/19.
+//  Copyright © 2019 angoma. All rights reserved.
+//
+
+import Foundation
+
+class Tarea {
+    var nombre = ""
+    var importante = false
+}
---
+++ laboratorio05Angoma/laboratorio05AngomaTests/laboratorio05AngomaTests.swift
@@ -0,0 +1,34 @@
+//
+//  laboratorio05AngomaTests.swift
+//  laboratorio05AngomaTests
+//
+//  Created by Arian Angoma on 5/04/19.
+//  Copyright © 2019 angoma. All rights reserved.
+//
+
+import XCTest
+@testable import laboratorio05Angoma
+
+class laboratorio05AngomaTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
